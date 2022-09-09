4ff44b890e318abe49da47df6ea1db520dc8c635
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-09-09 18:41:56 +0200

    created commonjs module in module.js and used in scritp.js

--- Es_02.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-
-  const randomID = crypto.randomUUID()
-
-  console.log(randomID)
---
+++ Es_03/script.js
@@ -0,0 +1,3 @@
+const outputMessage = require("./module")
+
+outputMessage("Hello there!")
