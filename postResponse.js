eca7043b329473eb37a51702eacecea71c22e9b1
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-09-12 01:32:15 +0200

    inspected the HTTP response with curl

---
+++ postResponse.js
@@ -0,0 +1,3 @@
+// Used the command: "curl https://jsonplaceholder.typicode.com/posts/1/comments" -v to get the verbose response from HTTP request
+
+// The value of the content-type reponse header is:"content-type: application/json; charset=utf-8"
