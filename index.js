1ced9d89404730d97cd21d517b48ddad85a71202
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-09-12 04:33:54 +0200

    Create async and await function getResult and call inside luckyDraw function

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 function luckyDraw(player) {
-    return new Promise((resolve, reject) => {
-      const win = Boolean(Math.round(Math.random()));
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
 
-      process.nextTick(() => {
-        if (win) {
-          resolve(`${player} won a prize in the draw!`);
-        } else {
-          reject(new Error(`${player} lost the draw.`));
-        }
-      });
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
     });
-  }
+  });
+}
 
-  luckyDraw("Joe")
-  .then((response) => console.log(response))
-  .then(() => luckyDraw("Caroline"))
-  .then((response) => console.log(response))
-  .then(() => luckyDraw("Sabrina"))
-  .then((response) => console.log(response))
-  .catch((reject) => console.log(reject))
+async function getResults() {
+  try {
+    const data = await Promise.all([
+      await luckyDraw("Tina"),
+      await luckyDraw("Jorge"),
+      await luckyDraw("Julien"),
+    ]);
+
+    console.log("data: ", data);
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getResults();
