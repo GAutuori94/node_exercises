b91fa68b4ebe726c37b19853b23befba35ca8b0b
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-09-12 02:19:47 +0200

    integration test

--- postResponse.js
+++
@@ -1,3 +0,0 @@
-// Used the command: "curl https://jsonplaceholder.typicode.com/posts/1/comments" -v to get the verbose response from HTTP request
-
-// The value of the content-type reponse header is:"content-type: application/json; charset=utf-8"
